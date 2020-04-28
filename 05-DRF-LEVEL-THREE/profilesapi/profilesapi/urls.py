ed7c021ddaea924f56a6a683674af62520d0dd69
B Ticehurst <40832194+BTicehurst@users.noreply.github.com>
2020-04-28 15:34:24 +0100

    Add files via upload

--- 05-DRF-LEVEL-THREE/profilesapi/profilesapi/urls.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profilesapi/urls.py
@@ -1,7 +1,7 @@
 """profilesapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,17 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_auth.views import LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("profiles.api.urls")),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api/rest-auth/", include("rest_auth.urls")),
-    path("api/rest-auth/registration/", include("rest_auth.registration.urls"))
-]
-
-from django.conf.urls.static import static
-from django.conf import settings
+    path("api/", include('profiles.api.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
+    path('api/rest-auth/', include('rest_auth.urls')),
+    path('api/rest-auth/registration/', include('rest_auth.registration.urls'))
+] 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
